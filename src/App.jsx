3b7550e51712c6c5f9d73f5ab8df53349d16f866
Imanifari <imaniahweb@gmail.com>
2025-09-05 18:02:03 -0700

    Add new SVG icons and implement responsive header component
    - Added triangle.svg, xora.svg, and zap.svg icons to the public/images directory.
    - Created a responsive Header component in Header.jsx with navigation links and a toggle button for mobile view.
    - Integrated the new SVG icons into the Header component for branding and navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./sections/Header";
 
 function App() {
   return (
-    <>
-      <h1>welcome</h1>
-    </>
+    <main className="overflow-hidden">
+      <Header />
+    </main>
   );
 }
 
---
+++ src/sections/Header.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Link as LinkScroll } from "react-scroll";
+const Navlink = ({ title }) => (
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
+          <img src="/images/xora.svg" alt="logo" width={115} height={55} />
+        </a>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0 ">
+          <div className=" max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-8">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <Navlink title="features" />
+                  <div className="dot"></div>
+                  <Navlink title="pricing" />
+                </li>
+                <li>
+                  <LinkScroll>
+                    <img
+                      src="/images/xora.svg"
+                      alt="logo"
+                      width={160}
+                      height={55}
+                    />
+                  </LinkScroll>
+                </li>
+                <li className="nav-li">
+                  <Navlink title="faq" />
+                  <div className="dot"></div>
+                  <Navlink title="download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+        >
+          <img
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            alt="borger icon"
+          />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
