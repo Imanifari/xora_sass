537111ee29246ffe4f985e37880912853a6e8aab
Imanifari <imaniahweb@gmail.com>
2025-09-09 18:09:00 -1100

    feat: implement Download section and enhance TestimonialItem component with detailed layout

---
+++ src/sections/Download.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Element } from "react-scroll";
+const Download = () => {
+  return (
+    <section>
+      <Element name="download" className="g7 relative pb-32 max-lg:34 max-md:">
+        {" "}
+        <div></div>
+      </Element>
+    </section>
+  );
+};
+
+export default Download;
