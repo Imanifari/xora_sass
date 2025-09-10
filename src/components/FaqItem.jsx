a43e38f0a1a55d41b1a81fa7f92b412ce0f26e30
Imanifari <imaniahweb@gmail.com>
2025-09-10 12:35:06 -1100

    feat: add Footer section and enhance Download section with improved layout and content

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -19,7 +19,7 @@ const FaqItem = ({ item, index }) => {
           </div>
           <div
             className={clsx(
-              "h-6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center",
+              "h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center",
               active && "max-lg:text-p1"
             )}
           >
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section
       className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36
-  max-md:pb-32 border-2 border-amber-400"
+  max-md:pb-32"
     >
       <Element name="hero">
         <div className="container">
@@ -16,7 +16,7 @@ const Hero = () => {
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
               Amazingly simple
             </h1>
-            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+            <p className="max-w-440 mb-14 max-sm:text-xl max-sm:w-[320px] body-1 max-md:mb-10">
               We designed XORA AI Video Editor to be an easy to use, quick to
               learn, and surprisingly powerful.
             </p>
@@ -24,7 +24,7 @@ const Hero = () => {
               <Button icon="/images/zap.svg">Try it now </Button>
             </LinkScroll>
           </div>
-          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img-res">
+          <div className="absolute -top-32 max-sm:-top-[70px]  left-[calc(50%-340px)] max-sm:left-[calc(50%-370px)] w-[1230px] pointer-events-none hero-img-res">
             <img
               src="/images/hero.png"
               alt=""
